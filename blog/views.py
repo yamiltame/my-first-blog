ee19f5d12e0bfbadcbcde4b3c8cb6df55080ad6b
yamil <yamiltamesoria@gmail.com>
2019-08-22 15:21:24 -0500

    shit

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-#urlpatterns = [
-#    url('', views.post_list, name='post_list'),
-#]
 
 def post_list(request):
 	return render(request,'blog/post_list.html',{})
